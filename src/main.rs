dee6e6aeb6876f646d39353a139fee0ecd0a3770
Ken Youens-Clark <kyclark@gmail.com>
2021-08-12 12:35:35 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::process;
+
+fn main() {
+    let config = match change::get_args() {
+        Ok(c) => c,
+        Err(e) => {
+            eprintln!("{}", e);
+            process::exit(1);
+        }
+    };
+
+    if let Err(e) = change::run(config) {
+        eprintln!("Error: {}", e);
+        process::exit(1);
+    }
+}
